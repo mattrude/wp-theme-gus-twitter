a5f92b0660c39916d111ffa16acdf5b2539a8863
Matt Rude <matt@mattrude.com>
2013-03-16 20:54:55 -0500

    updating to current

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
         <a class="standard" href="<?php bloginfo('url'); ?>"><b><?php bloginfo('description'); ?></b></a>
       </div>
     </div>
-    <div id="header">
+<!--    <div id="header">
       <img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
-    </div>
+    </div>-->
   <div id="container">
