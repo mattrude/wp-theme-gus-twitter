292b52bdd3eb36d21827b03e56052d1e5a9c500d
Matt Rude <matt@mattrude.com>
2013-03-23 13:22:34 -0500

    Added phpdoc section to header.php, see #3

--- header.php
+++ header.php
@@ -1,3 +1,9 @@
+<?php
+/**
+ * This is the main header template of the theme
+ * @package Gus Twitter
+ */
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
