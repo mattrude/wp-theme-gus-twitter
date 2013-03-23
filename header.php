978c912be69bed9f76c850c6923a57dccc3d1bfb
Matt Rude <matt@mattrude.com>
2013-03-23 13:40:22 -0500

    Minor updates to header.php footer.php php footers

--- header.php
+++ header.php
@@ -34,3 +34,5 @@
       <img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
     </div>-->
   <div id="container">
+<?php
+?>
