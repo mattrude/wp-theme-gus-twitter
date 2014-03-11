8bf6b82c22b5b06919193c8447b37c0d4a1d0dbc
Matt Rude <matt@mattrude.com>
2014-03-11 13:14:29 -0500

    Remove changeable header functions and image

--- functions.php
+++ functions.php
@@ -12,21 +12,6 @@
 add_theme_support( 'automatic-feed-links' );
 
 /**
- * Your changeable header business starts here
- *
- * @since 0.1
- * @package Gus Twitter
- */
-$custom_header_support = array(
-    'default-image' => '%s/images/header-cabin.jpg',
-    'default-text-color' => '000',
-    'header-text' => false,
-    'width' => 700,
-    'height' => 143,
-);
-add_theme_support( 'custom-header', $custom_header_support );
-
-/**
  * Add theme support for infinite scroll
  *
  * @uses add_theme_support
