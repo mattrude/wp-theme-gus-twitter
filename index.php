7a539b21459ef329627cc2643d00daec3190de2d
Matt Rude <matt@mattrude.com>
2014-03-11 13:59:24 -0500

    Move 404.php into index.php

--- index.php
+++ index.php
@@ -7,10 +7,17 @@
 get_header(); ?>
 
 <div id='content'>
-<?php $screename = 'mdrude';
-  while (have_posts()) : the_post();
-    get_template_part( 'content', get_post_format() );
-  endwhile; ?>
+    <?php $screename = 'mdrude';
+    if (have_posts()) {
+        while (have_posts()) : the_post();
+            get_template_part( 'content', get_post_format() );
+        endwhile;
+    } else { ?>
+        <div class="page">
+            <h2 class="center"><center>Oops! - 404 Error</center></h2> 
+            <p class="center"><center>No sense sticking around here for very long.</center></p>
+        </div>
+    <?php } ?>
 </div>
 
 <?php get_footer(); ?>
